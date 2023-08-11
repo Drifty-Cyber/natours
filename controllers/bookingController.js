7c4357d8adcd38955865be31c9a252907e3391c0
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-08-11 15:46:49 +0100

    Improved hosted image

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -14,7 +14,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const product = await stripe.products.create({
     name: `${tour.name}`,
     description: `${tour.description}`,
-    images: [`${req.protocol}://${req.get('host')}/img/tours/tour-1-cover.jpg`],
+    images: [`${req.protocol}://${req.get('host')}/img/tours/tour-3-cover.jpg`],
   });
 
   // Define a price object
