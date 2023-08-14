ab330c72475058c7b6481c6c35087f6244aaa938
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-08-14 18:41:04 +0100

    Improved z-index

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -68582,7 +68582,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55454" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55845" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
